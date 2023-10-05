5530254ffe9b4ae1477a12918fd4790c8f3addd7
Paloma~ <palomasol201@gmail.com>
2023-10-05 12:24:24 -0300

    add client rpc

--- scenes/cliente.gd
+++ scenes/cliente.gd
@@ -7,11 +7,18 @@ extends Node2D
 var selected = false
 
 func _on_area_2d_input_event(viewport, event, shape_idx):
-	if !Input.is_action_pressed("right_click"):
-		selected = false
-	else:
-		selected = true
-		
+	if is_multiplayer_authority():
+		if !Input.is_action_pressed("right_click"):
+			if selected:
+				selected = false
+				send_position.rpc(position)
+		else:
+			selected = true
+
+@rpc("authority", "reliable")
+func send_position(pos):
+	position = pos
+	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var id = multiplayer.get_unique_id()
@@ -20,7 +27,9 @@ func _ready():
 	Debug.dprint(role)
 	if role == 1:
 		get_node("Area2D/CollisionShape2D").disabled = true
-		
+	for p in Game.players:
+		if p.role == 2:
+			set_multiplayer_authority(p.id)
 	pass # Replace with function body.
 
 
--- scenes/player.gd
+++ scenes/player.gd
@@ -19,23 +19,12 @@ func _physics_process(delta):
 		if position.distance_to(target) > 10:
 			move_and_slide()
 		
-#		if Input.is_action_pressed("right_click"):
-#			var mousePos = get_global_mouse_position()
-#			var space = get_world_2d().direct_space_state
-#			var collision_objects = space.intersect_point(mousePos, 1)
-#			if collision_objects:
-#				if collision_objects is Cliente:
-#					Debug.dprint(collision_objects[0].collider.name)
-#			else:
-#				Debug.dprint("no hit")
-		
 func setup(player_data: Game.PlayerData):
 	set_multiplayer_authority(player_data.id)
 	name = str(player_data.id)
 	Debug.dprint(player_data.name, 30)
 	Debug.dprint(player_data.role, 30)
 	pause_menu.set_multiplayer_authority(player_data.id)
-
 	
 @rpc
 func test():
