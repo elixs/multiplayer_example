b5b86d97dd8515ed5cd1545898f01838c08f546a
RIturrieta <rodrigo.iturrieta@ug.uchile.cl>
2024-04-10 21:11:56 -0400

    Free camera movement

--- scripts/BaseCharacter.gd
+++ scripts/BaseCharacter.gd
@@ -10,44 +10,42 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 @onready var agent = $NavigationAgent3D
 var target: Vector3
 
-@onready var camera_3d = $Camera3D
+@onready var camera_3d = $Path3D/PathFollow3D/Camera3D
+@onready var path_follow_3d = $Path3D/PathFollow3D
+@onready var path_3d = $Path3D
+
+var camera_target_pos = 0.0
 @onready var animation_player = $AnimationPlayer
-@onready var arrows = $Arrows
+@onready var arrows = $PathArrows
 @onready var arrows_transform = $ArrowsTransform
 
+var locked_camera = true
+@onready var camera_transform = $CameraTransform
+
+var camera_follow_speed = 0.6
+# var screen_size: Vector2
+
 func _ready():
+	# Esto se debe cambiar dsps, por obtener el tamaño desde las settings
+	# screen_size = get_viewport().get_visible_rect().size
 	pass
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
 		
-	look_at(target)
+		
+	# look_at(target)
 	rotation.x = 0
 	rotation.y = 0
 	if Input.is_action_pressed("Move"):
 		target = screenPointToRay()
 		if Input.is_action_just_pressed("Move"):
-			target.y = 0.2
+			target.y = 0.1
 			arrows_transform.global_position = target
 			animation_player.play("move_arrows")
-		target.y = 0
+		target.y = -0.5
 		updateTargetLocation(target)
 	if position.distance_to(target) > 0.5:
 		var current_position = global_transform.origin
@@ -55,8 +53,37 @@ func _physics_process(delta):
 		var new_velocity = (target_position - current_position).normalized() * SPEED
 		velocity = new_velocity
 		move_and_slide()
-
+	if Input.is_action_just_pressed("Release Camera"):
+		if locked_camera:
+			locked_camera = false
+			path_3d.top_level = true
+			camera_transform.update_position = false
+			camera_transform.top_level = true
+		else:
+			locked_camera = true
+			path_3d.top_level = false
+			# path_3d.global_position = global_transform.origin
+			camera_transform.global_position = global_transform.origin
+			camera_transform.update_position = true
+			camera_transform.top_level = false
+			# path_3d.top_level = false
+			print("Lock")
+		print("Pos difference:", global_transform.origin - path_3d.global_position)
+	
+	path_follow_3d.progress_ratio = lerp(path_follow_3d.progress_ratio, camera_target_pos, 0.2)
 	#move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseButton:
+		var ratio = path_follow_3d.progress_ratio
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP and event.pressed and ratio < 1:
+			camera_target_pos = min(ratio + 0.1, 1.0)
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN and event.pressed and ratio > 0:
+			camera_target_pos = max(ratio - 0.1, 0.0)
+		   # Mouse in viewport coordinates.
+	elif event is InputEventMouseMotion:
+		print("Mouse Motion at: ", event.position)
+		moveCameraByCursor(event.position)
 	
 func screenPointToRay():
 	var space_state = get_world_3d().direct_space_state
@@ -71,3 +98,23 @@ func screenPointToRay():
 	
 func updateTargetLocation(target):
 	agent.target_position = target
+	
+func moveCameraByCursor(position: Vector2):
+	if !locked_camera:
+		var screen_size = get_viewport().get_visible_rect().size
+		var screenX = screen_size.x
+		var screenY = screen_size.y
+		var dir = Vector2(0.0, 0.0)
+		if screenX - position.x < 11:
+			dir += Vector2(camera_follow_speed, 0.0)
+			print("Der")
+		elif position.x < 11:
+			dir += Vector2(-camera_follow_speed, 0.0)
+			print("Izq")
+		elif screenY - position.y < 11:
+			dir += Vector2(0.0, camera_follow_speed)
+			print("Up")
+		elif position.y < 11:
+			dir += Vector2(0.0, -camera_follow_speed)
+			print("Dwn")
+		path_3d.global_position += Vector3(dir.x, 0.0, dir.y)
