06b295e778621707447c3559c9f7129739f02162
DiegoSanma <diego.sanmartin.a@ug.uchile.cl>
2024-08-28 13:37:40 -0400

    feat: Added crouching to the game, as well as changing the sprite

--- scenes/player_1.gd
+++ scenes/player_1.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
+@onready var sprite: Sprite2D = $Sprite2D
+@onready var collision_shape: CollisionShape2D = $CollisionShape2D
 
 var SPEED = 300.0
 var JUMP_VELOCITY = 400.0
 var ACCELERATION = 1000
+var jumps = 0
 
 var player
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -17,17 +20,24 @@ func setup(player_data: Statics.PlayerData) -> void:
 
 func _input(event:InputEvent) -> void:
 	if is_multiplayer_authority():
-		pass
+		if Input.is_action_just_pressed("crouch"):
+			sprite.frame = 20
+		if Input.is_action_just_released("crouch"):
+			sprite.frame = 0	
+			
 		
 
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
 		var move_input = Input.get_axis("move_left","move_right")
 		velocity.x = move_toward(velocity.x, SPEED* move_input, ACCELERATION * delta)
-		if Input.is_action_just_pressed("jump"):
+		if Input.is_action_just_pressed("jump") and jumps <2:
 			velocity.y= -JUMP_VELOCITY
+			jumps+=1
 		if not is_on_floor():
 			velocity.y += gravity * delta
+		else:
+			jumps = 0	
 		move_and_slide()
 		send_position.rpc(position,velocity)
 
