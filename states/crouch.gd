4fda61926ea335445279c6f0f2a9bd1fe785e8fb
DiegoSanma <diego.sanmartin.a@ug.uchile.cl>
2024-09-04 10:21:18 -0400

    feat: Added crouch animation to crouch state

--- states/crouch.gd
+++ states/crouch.gd
@@ -2,11 +2,13 @@ extends State
 
 @export var moving_state: State
 @export var idle_state: State
+@export var crouch_animation: AnimationPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func enter() -> void:
-	parent.update_sprite(20)
+	crouch_animation.play("crouch")
+	parent.rpc("send_animation","crouch")
 	
 func update(event:InputEvent) -> State:
 	if event.is_action_pressed("move_left") or event.is_action_pressed("move_right"):
