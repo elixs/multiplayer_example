fc15481d951d61baabe84c18d896ba56d6b83810
WinterNacho <winternacho@gmail.com>
2024-10-01 15:15:22 -0300

    #19 Configuracion lobbies con nuevo mapa

--- scenes/lobby.gd
+++ scenes/lobby.gd
@@ -220,7 +220,7 @@ func starting_game(value: bool):
 @rpc("any_peer", "call_local", "reliable")
 func start_game() -> void:
 	Game.players.sort_custom(func(a, b): return a.index < b.index)
-	get_tree().change_scene_to_file("res://scenes/mapa1.tscn")
+	get_tree().change_scene_to_file("res://scenes/map/map.tscn")
 
 
 func _disconnect():
