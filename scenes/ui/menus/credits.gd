a7333f16792e1820b6891eda4a9fbee7f43f089f
WinterNacho <winternacho@gmail.com>
2024-12-04 20:39:33 -0300

    Agregar algunos creditos

--- scenes/ui/menus/credits.gd
+++ scenes/ui/menus/credits.gd
@@ -1,7 +1,7 @@
 extends Control
 
 @onready var main_menu = "res://scenes/ui/menus/main_menu.tscn"
-@onready var back_button: Button = $MarginContainer/VBoxContainer/BackButton
+@onready var back_button: Button = $MarginContainer/VBoxContainer/HBoxContainer/BackButton
 
 func _ready() -> void:
 	back_button.button_down.connect(back)
