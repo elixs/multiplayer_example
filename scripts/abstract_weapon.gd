3dba73373f3edfcf36140aff748f6282904f188c
Matias Cisterna <matiasjesus300@gmail.com>
2025-04-25 13:36:16 -0400

    init commit

--- scripts/abstract_weapon.gd
+++ scripts/abstract_weapon.gd
@@ -2,7 +2,10 @@ extends Node2D
 var bullet_path:String
 @export var type_bullet:String = 'boomerang'
 @onready var bullet_scene = load("res://scenes/projectiles/bullet_"+type_bullet+".tscn")
-
+@onready var sprite_base = $SpriteBase
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree["parameters/playback"]
+@onready var bullet_force = 0
 @rpc("authority","call_local","reliable")
 func shoot():
 	var bullet = bullet_scene.instantiate() 
@@ -10,4 +13,7 @@ func shoot():
 	#print(get_parent().rotation)
 	bullet.rotacion_spawn = get_parent().global_rotation
 	get_parent().get_parent().get_parent().add_child(bullet,true)
-	
+func charge():
+	print("charge")
+	playback.travel("charge")
+	 
