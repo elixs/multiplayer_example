bcaec602e635b13c3caaf6e428693c41ca5b8158
Matias Cisterna <matiasjesus300@gmail.com>
2025-05-09 13:39:26 -0400

    feat: weapon_charge with indicators and power

--- scripts/abstract_weapon.gd
+++ scripts/abstract_weapon.gd
@@ -6,14 +6,29 @@ var bullet_path:String
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree["parameters/playback"]
 @onready var bullet_force = 0
+@onready var is_overcharged = false
+@onready var overcharge_sprite = $overcharge_sprite
+
 @rpc("authority","call_local","reliable")
-func shoot():
-	var bullet = bullet_scene.instantiate() 
+func shoot(charge_power):
+	playback.travel("idle")
+	var bullet:proyectile = bullet_scene.instantiate() 
+	overcharge_sprite.visible = false
+
 	bullet.pos_spawn = $Marker2D.global_position
 	#print(get_parent().rotation)
 	bullet.rotacion_spawn = get_parent().global_rotation
+	bullet.speed = charge_power * bullet.speed
 	get_parent().get_parent().get_parent().add_child(bullet,true)
-func charge():
-	print("charge")
+	if is_overcharged:
+		var scala = Vector2(3.0,3.0)
+		bullet.animation.scale = scala
+		bullet.zona_colision.scale = scala
+	is_overcharged = false
+
+func begin_charge():
 	playback.travel("charge")
+func overcharge():
+	overcharge_sprite.visible = true
+	is_overcharged = true
 	 
