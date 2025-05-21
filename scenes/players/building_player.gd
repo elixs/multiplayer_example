bd49cdd6475cd73794e0c6e6166b657a3ade3067
Matías Jesús Cisterna Nauto <matiasjesus300@gmail.com>
2025-05-21 14:08:40 -0400

    Feat: Manejo total de fase construccion e inicio juego

--- scenes/players/building_player.gd
+++ scenes/players/building_player.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var object_scene: PackedScene
 @onready var sprite = $Sprite2D
 @onready var object_preview = $ObjectPreview
+@onready var label: Label = $Label
 
 var placed_object: Node = null
 var is_active = true
@@ -14,7 +15,7 @@ func _ready():
 		placed_object.visible = false
 
 func _process(_delta):
-	if is_active:
+	if is_active and is_multiplayer_authority():
 		global_position = get_global_mouse_position()
 
 		if Input.is_action_pressed("right_click"):
@@ -27,8 +28,14 @@ func _process(_delta):
 				placed_object.visible = false
 
 		if Input.is_action_just_pressed("left_click") and placed_object:
-			place_object()
-
+			place_object.rpc()
+			
+func setup(player_data: Statics.PlayerData):
+	label.text = player_data.name
+	name = str(player_data.id)
+	set_multiplayer_authority(player_data.id)
+	
+@rpc("authority", "call_local")
 func place_object():
 	is_active = false
 
