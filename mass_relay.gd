27d60dfa4fe279fcc498ae6aa2ca7f7085ddbdb9
marianomorahoffmann <mariano.mora@ug.uchile.cl>
2025-05-29 20:26:53 -0400

    Fix: mass relay

--- mass_relay.gd
+++ mass_relay.gd
@@ -2,41 +2,41 @@ extends Area2D
 
 @onready var timer: Timer = $Timer
 @onready var animator: AnimationPlayer = $AnimationPlayer
-
+var is_throwing = false
 func _ready() -> void:
 	connect("body_entered", Callable(self, "_on_body_entered"))
 	
 	
 func _on_body_entered(body):
-	print("body entered")
-	if body.is_in_group("throwable"):
-		print(" is throwable")
+	if body.is_in_group("throwable") and not is_throwing:
+		is_throwing = true
 		throw(body)
-	else:
-		print("not throwable")
 
 func throw(target):
-	# Oculta el personaje
-	target.visible = false
-	
-	# Detiene la animación de rotación
-	animator.stop(true)
-	var launch_direction = global_transform.y.normalized()
-	var force = 1000  # Ajusta la fuerza según necesites
-	# Espera 1 segundo
-	timer.start(1.0)
+	animator.pause()
+	target.linear_velocity = Vector2.ZERO  # Detiene movimiento si tiene
 
+	# Reubica el objeto al centro del Mass Relay
+	print("Antes de mover: ", target.global_position)
+	target.call_deferred("set_global_position", global_position)
+	#print("Intentando mover a: ", global_position)
+
+	#await get_tree().process_frame  # Espera al siguiente frame para ver si se "revierte"
+	#print("Después de mover (1 frame): ", target.global_position)
+
+	var launch_direction = -global_transform.y.normalized()
+	var force = 1000
+	timer.start(0.7)
 	await timer.timeout
-	# Lanza al personaje
 	if target.has_method("apply_central_impulse"):
 		target.apply_central_impulse(launch_direction * force)
 	elif target.has_method("set_linear_velocity"):
 		target.set_linear_velocity(launch_direction * force)
 	else:
-		target.global_position += launch_direction * 100  # Fallback simple
-	
-	# Vuelve a hacerlo visible
-	target.visible = true
-	
-	# Reanuda la animación de rotación
+		target.global_position += launch_direction * 100
+	timer.start(0.3)
+	await timer.timeout
+
 	animator.play("rotate")
+	is_throwing = false
+	
