a315537fdf97b177da34fd886f7cfb183ddb2a34
Matías Jesús Cisterna Nauto <matiasjesus300@gmail.com>
2025-07-08 12:46:43 -0400

    Feat: Box structure, and box_cannon

---
+++ scenes/structures/box_cannon.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+@export var box_scene: PackedScene = preload("res://scenes/structures/damageable_block.tscn")
+@export var fire_speed := 600.0  # Magnitud de la velocidad
+@export var fire_interval := 1.0
+@export var authority_peer := 1  # Generalmente 1 = servidor
+
+@onready var spawn_point: Marker2D = $SpawnPoint
+
+func _ready():
+	if is_multiplayer_authority():
+		_fire_loop()
+	else:
+		set_process(false)
+
+func _fire_loop():
+	await get_tree().create_timer(fire_interval).timeout
+	fire_box()
+	_fire_loop()
+
+func fire_box():
+	var box = box_scene.instantiate()
+
+	box.affected_by_gravity = true
+	box.infinite_health = false
+
+	box.position = spawn_point.global_position
+	box.set_multiplayer_authority(authority_peer)
+	get_tree().current_scene.add_child(box)
+
+	if box is RigidBody2D:
+		var direction = Vector2.RIGHT.rotated(global_rotation)
+		box.linear_velocity = direction * fire_speed
---
+++ scenes/structures/damageable_block.gd
@@ -0,0 +1,33 @@
+extends RigidBody2D
+
+@export var affected_by_gravity := true
+@export var infinite_health := false
+
+var alive := true
+
+func _ready():
+	if not affected_by_gravity:
+		gravity_scale = 0
+	else:
+		gravity_scale = 1
+
+@rpc("authority", "call_local", "reliable")
+func recieve_damage():
+	if not alive:
+		return
+
+	if infinite_health:
+		print("Bloque ignoró daño (vida infinita)")
+		return
+
+	print("Bloque destruido por daño")
+	explode()
+
+func explode():
+	alive = false
+	send_state.rpc(alive)
+	queue_free()
+
+@rpc("authority")
+func send_state(state: bool):
+	alive = state
